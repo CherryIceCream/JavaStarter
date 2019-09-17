d71a8411cdb5b0c384c7fa5f8bb206542f688e57
CherryIceCream <raul91@bk.ru>
2019-09-17 20:15:37 +0300

    Задача №5: Таблица умноженияЗадание Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено.

---
+++ src/Lesson4/test5.java
@@ -0,0 +1,27 @@
+package Lesson4;
+
+public class test5 {
+   public static void main(String[] args) {
+       int z = 5;
+       int a1 = z*1;
+       int a2 = z*2;
+       int a3 = z*3;
+       int a4 = z*4;
+       int a5 = z*5;
+       int a6 = z*6;
+       int a7 = z*7;
+       int a8 = z*8;
+       int a9 = z*9;
+       int a10 = z*10;
+       System.out.println(a1);
+       System.out.println(a2);
+       System.out.println(a3);
+       System.out.println(a4);
+       System.out.println(a5);
+       System.out.println(a6);
+       System.out.println(a7);
+       System.out.println(a8);
+       System.out.println(a9);
+       System.out.println(a10);
+   }
+}
