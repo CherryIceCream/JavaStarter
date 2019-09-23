cad4ac3486b9f3a5bcbdfd07e11fa9416c87f109
CherryIceCream <raul91@bk.ru>
2019-09-23 19:16:57 +0300

    Переделал под константы и свич

--- src/Lesson5/test7.java
+++ src/Lesson5/test7.java
@@ -3,21 +3,27 @@ package Lesson5;
 import java.util.Scanner;
 
 public class test7 {
+    private static final int X = 5;
+    private static final int Y = 229;
+    private static final int Z = 667;
+
     public static void main (String[] args) {
 
         Scanner s = new Scanner(System.in);
         System.out.println("Введи число: ");
         int number = s.nextInt();
-        int X = 5;
-        int Y = 156;
-        int Z = 229;
-        if (number == X) {
-            System.out.println("Данное значение имеется в константах");
-        } else if (number == Y) {
-            System.out.println("Данное значение имеется в константах");
-        } else if (number == Z) {
-            System.out.println("Данное значение имеется в константах");
-        } else {
-            System.out.println("Такой константы нет");}
+
+        String a1;
+        switch (number) {
+            case X:
+            case Y:
+            case Z:
+                a1 = "Данное значение имеется в константах";
+            break;
+            default: a1 = "Такой константы нет!";
+            break;
+        }
+        System.out.println(a1);
         }
     }
+
