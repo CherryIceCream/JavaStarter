8db5de023059830e421cdf9d16c3d2a07e454a9e
CherryIceCream <raul91@bk.ru>
2019-10-10 20:41:29 +0300

    Задание №9: Ввод/вывод данных для массива Произведите ввод данных с клавиатуры в массив, а после этого произведите вывод массива на экран, где каждый элемент массива умножается на 2. Размер массива задается пользователем.

---
+++ src/Lesson6/test9.java
@@ -0,0 +1,24 @@
+package Lesson6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test9 {
+    public static void main(String[] args){
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введи длину массива");
+        n = in.nextInt();
+
+        int[] mas =new int[n];
+        int[] mas2 = new int[n];
+        System.out.println("Введи " +n+ "чисел построчно");
+        for (int i=0; i<n; i++) {
+        Scanner in2 = new Scanner(System.in);
+        mas[i] = in2.nextInt();
+        mas2[i]=2*mas[i];
+        }
+        System.out.println("Получи массив с х2 числами"+Arrays.toString(mas2));
+    }
+}
+
